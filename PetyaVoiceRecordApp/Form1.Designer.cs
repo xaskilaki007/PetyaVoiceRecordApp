8631b1b05e64aa64bbe8e20f9dc744b13edbaf7c
Пëтр <115955213+xaskilaki007@users.noreply.github.com>
2025-03-25 22:40:16 +0300

    Добавьте файлы проекта.

---
+++ PetyaVoiceRecordApp/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace PetyaVoiceRecordApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonRecord = new System.Windows.Forms.Button();
+            this.ButtonSave = new System.Windows.Forms.Button();
+            this.ButtonPlay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.IbITimer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ButtonRecord
+            // 
+            this.ButtonRecord.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(33)))), ((int)(((byte)(35)))));
+            this.ButtonRecord.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ButtonRecord.FlatAppearance.BorderSize = 5;
+            this.ButtonRecord.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ButtonRecord.Font = new System.Drawing.Font("Segoe Script", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonRecord.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(200)))), ((int)(((byte)(49)))));
+            this.ButtonRecord.Location = new System.Drawing.Point(24, 47);
+            this.ButtonRecord.Name = "ButtonRecord";
+            this.ButtonRecord.Size = new System.Drawing.Size(115, 66);
+            this.ButtonRecord.TabIndex = 0;
+            this.ButtonRecord.Text = "Record";
+            this.ButtonRecord.UseVisualStyleBackColor = false;
+            // 
+            // ButtonSave
+            // 
+            this.ButtonSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ButtonSave.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ButtonSave.FlatAppearance.BorderSize = 5;
+            this.ButtonSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ButtonSave.Font = new System.Drawing.Font("Times New Roman", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonSave.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(20)))), ((int)(((byte)(49)))));
+            this.ButtonSave.Location = new System.Drawing.Point(159, 71);
+            this.ButtonSave.Name = "ButtonSave";
+            this.ButtonSave.Size = new System.Drawing.Size(115, 42);
+            this.ButtonSave.TabIndex = 1;
+            this.ButtonSave.Text = "Save";
+            this.ButtonSave.UseVisualStyleBackColor = false;
+            // 
+            // ButtonPlay
+            // 
+            this.ButtonPlay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ButtonPlay.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ButtonPlay.FlatAppearance.BorderSize = 15;
+            this.ButtonPlay.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ButtonPlay.Font = new System.Drawing.Font("Times New Roman", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonPlay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(20)))), ((int)(((byte)(49)))));
+            this.ButtonPlay.Location = new System.Drawing.Point(291, 71);
+            this.ButtonPlay.Name = "ButtonPlay";
+            this.ButtonPlay.Size = new System.Drawing.Size(129, 42);
+            this.ButtonPlay.TabIndex = 2;
+            this.ButtonPlay.Text = "Play➤";
+            this.ButtonPlay.UseVisualStyleBackColor = false;
+            this.ButtonPlay.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PetyaVoiceRecorder";
+            // 
+            // IbITimer
+            // 
+            this.IbITimer.AutoSize = true;
+            this.IbITimer.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IbITimer.Location = new System.Drawing.Point(154, 1);
+            this.IbITimer.Name = "IbITimer";
+            this.IbITimer.Size = new System.Drawing.Size(274, 25);
+            this.IbITimer.TabIndex = 4;
+            this.IbITimer.Text = "TextDuration :00:00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 153);
+            this.Controls.Add(this.IbITimer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ButtonPlay);
+            this.Controls.Add(this.ButtonSave);
+            this.Controls.Add(this.ButtonRecord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonRecord;
+        private System.Windows.Forms.Button ButtonSave;
+        private System.Windows.Forms.Button ButtonPlay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label IbITimer;
+    }
+}
+
